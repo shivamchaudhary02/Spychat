6b3fffa9f511fe0398aa6964171d9491a467d3de
shubham <shubhamektarfa@gmail.com>
2018-03-23 21:44:39 +0530

    First commit of input of name and salutation in spychat

---
+++ main.py
@@ -0,0 +1,9 @@
+print 'Hello'
+print 'Let\'s get Started'
+spy_name = raw_input('What is your Spy Name? ')
+print 'Welcome ' + spy_name+' Happy to see you again...'
+spy_salutation = raw_input('What should we call you Mr. or Ms.? ')
+print spy_salutation
+spy_name = spy_salutation + ' ' + spy_name
+print spy_name
+print 'Alright '+ spy_name +' I\'d like to know a little bit more about...' 
